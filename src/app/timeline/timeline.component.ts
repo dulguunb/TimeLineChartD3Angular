be5e17ce40efdc484d6dbdf7b504e7debf15fa3c
Dulgunn Bold <dulgunn.bold@ericsson.com>
2019-02-13 00:28:52 +0100

    Zoomed function and relevant properties and functions with its comments

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -16,30 +16,46 @@ export class TimelineComponent implements OnChanges {
   public data: TimeLineData[];
   @Input()
   public options: Option;
-  private svg: any;
-  private d3;
-  private el;
-  private x:any;
-  private xAxis:any;
+  @Input()
+  public verticalData: VerticalLine[];
+
+  private d3: D3;
+  private element: any;
+  private el: ElementRef;
   private now: any;
-  constructor(d3Service: D3Service, el: ElementRef) {
-    this.d3 = d3Service.getD3();
+  private static self;
+  private x: any;
+  private xAxis: any;
+  private svg: any;
+  private i = 0;
+  private height: any;
+  private width: any;
+  private groupWidth: any;
+  private new_scale: any;
+  public customStatusColor: any;
+  public customColors: any;
+  private statuses: any;
+  private verticalLines: any;
+  private groupLabels: any;
+
+  constructor(private d3Service: D3Service, el: ElementRef) {
     this.el = el;
   }
   ngOnChanges() {
+    this.d3 = this.d3Service.getD3();
     console.log(this.data);
     let allElements = this.data.reduce(
       (agg, e) =>
         agg.concat(e.data), []
     );
-    let element = this.el.nativeElement.children[0].children[0];
+    this.element = this.el.nativeElement.children[0].firstChild;
     // filtering array with the object that contains .data property 
     // allElements[]:SingleData. contains list of SingleData
     let minDt = this.d3.min(allElements, this.getPointMinDt);
     // maxDt is the same as the previous comment
     let maxDt = this.d3.max(allElements, this.getPointMaxDt);
-    let elementWidth  = this.options.width  || element.clientWidth;
-    let elementHeight = this.options.height || element.clientHeight;
+    let elementWidth  = this.options.width  || this.element.clientWidth;
+    let elementHeight = this.options.height || this.element.clientHeight;
     let margin = {
       top: 0,
       right: 0,
@@ -56,7 +72,7 @@ export class TimelineComponent implements OnChanges {
       .domain([minDt, maxDt])
       .range([groupWidth, width]);
 
-    this.xAxis = this.d3.axisBottom()
+    this.xAxis = this.d3.axisBottom(this.x)
       .scale(this.x)
       .tickSize(-height);
 
@@ -186,13 +202,13 @@ export class TimelineComponent implements OnChanges {
       .attr('r', 5);
 
     if (this.options.tip) {
-      if (this.d3.tip) {
-        let tip = this.d3.tip().attr('class', 'd3-tip').html(this.options.tip);
-        this.svg.call(tip);
-        dots.on('mouseover', tip.show).on('mouseout', tip.hide)
-      } else {
-        console.error('Please make sure you have d3.tip included as dependency (https://github.com/Caged/d3-tip)');
-      }
+      // if (this.d3.tip) {
+      //   let tip = this.d3.tip().attr('class', 'd3-tip').html(this.options.tip);
+      //   this.svg.call(tip);
+      //   dots.on('mouseover', tip.show).on('mouseout', tip.hide)
+      // } else {
+      //   console.error('Please make sure you have d3.tip included as dependency (https://github.com/Caged/d3-tip)');
+      // }
     }
     this.zoomed();
 
@@ -201,8 +217,81 @@ export class TimelineComponent implements OnChanges {
     }
 
   }
-  zoomed(): void {
+  zoomed = () => {
+    /*
+    this.x() scalling function is only created at the constructor and no use at the
+    zoomed() function when we define the zooming functionality.
+    Thus we need to create a new_scale function that will keep track of the zooming
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    *(To Verify the comment above: you can replace all of the occurances of new_scale function
+    with this.x() function and console.log(this.svg.select('.x.axis')) what you will see is that non-changed 
+    values of width and height int the rect element) *  
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    */
+    this.new_scale = this.d3.event.transform.rescaleX(this.x);
+    // scaling x Axis items - which are the span of dates
+    this.svg.select('.x.axis')
+      .call(this.xAxis
+        .scale(this.new_scale));
+    //scaling intervals
+    if (this.groupLabels) {
+      this.groupLabels
+        .attr('x1', d => { return this.new_scale(d.at) })
+        .attr('x2', d => { return this.new_scale(d.at) });
+    }
+    // this scaling increases and decreases the WIDTH of the WP's with
+    // relation of the .x-axis option.intervalMInWidth contains the number that can 
+    // scale if you zoom a lot then the invterval will dissappear
+    this.svg.selectAll('rect.interval')
+      .attr('x', d => this.new_scale(d.from))
+      .attr('width', d => Math.max(this.options.intervalMinWidth, this.new_scale(d.to) - this.new_scale(d.from)));
+    // scaling dots
+    this.svg.selectAll('circle.dot')
+      .attr('cx', d => this.new_scale(d.at));
+    /* scaling the texts that correpond to the interval (which are the WP's)
+       the zooming and scaling functionality have to hold <text> element
+       in relative terms with the intervals thus it has to also scale and/or zoom the texts
+    */
+    this.svg.selectAll('.interval-text')
+      .attr('x', (d, i, n) => {
+        // n[i] is refering to the DOM element
+        // this in D3 refers to the DOM element but in Angular it refers to the Object itself    
+        let positionData = this.getTextPositionData.call(this, d, n[i]);
+        // keyword "this" is refering to the object. d is the datum. n[i] is the element
+        if ((positionData.upToPosition - this.groupWidth - 10) < positionData.textWidth) {
+          return positionData.upToPosition;
+        } else if (positionData.xPosition < this.groupWidth && positionData.upToPosition > this.groupWidth) {
+          return this.groupWidth;
+        }
+        return positionData.xPosition;
+      }).attr('text-anchor', (d, i, n) => {
+        let positionData = this.getTextPositionData.call(this, d, n[i]);
+        /* keyword 'this' is refering to the object. d is the datum. 
+           n[i] is the element 
+        */
+        if ((positionData.upToPosition - this.groupWidth - 10) < positionData.textWidth) {
+          return 'end';
+        }
+        return 'start';
+      }).attr('dx', (d, i, n) => {
+        let positionData = this.getTextPositionData.call(this, d, n[i]);
+        if ((positionData.upToPosition - this.groupWidth - 10) < positionData.textWidth) {
+          return '-0.5em';
+        }
+        return '0.5em';
+      });
 
+  }
+  getTextPositionData(d, element) {
+    element.textSizeInPx = element.textSizeInPx || element.getComputedTextLength();
+    var from = this.new_scale(d.from);
+    var to = this.new_scale(d.to);
+    return {
+      xPosition: from,
+      upToPosition: to,
+      width: to - from,
+      textWidth: element.textSizeInPx
+    }
   }
   withCustom(defaultClass) {
     return d => d.customClass ? [d.customClass, defaultClass].join(' ') : defaultClass
